2e5070cf152be96e3fbd78f8b340405c5850c87d
Salomao Ferreira Junior <17413319608@campus.machado.mch>
2023-07-26 09:07:26 -0300

    Funcao BubbleSort - Passo a Passo

--- JavaSorts/src/javasorts/BubbleSort.java
+++ JavaSorts/src/javasorts/BubbleSort.java
@@ -1,5 +1,7 @@
 package javasorts;
 
+import java.util.Scanner;
+
 public class BubbleSort {
     
     public static void bSort(int array[]){
@@ -14,4 +16,22 @@ public class BubbleSort {
         }
     }
     
+    public static void bSortComentado(int array[]){
+        Scanner sc = new Scanner(System.in);
+        
+        for(int fase = 1; fase < array.length-1; fase++){
+            System.out.println("Fase ["+fase+"]");
+            JavaSorts.printArray(array);
+            sc.nextLine();
+            for(int comp=0; comp < array.length-fase; comp++){
+                System.out.println("Comparado ["+array[comp]+"] com ["+array[comp+1]+"]");
+                if(array[comp] > array[comp+1]){
+                    System.out.println("Trocou");
+                    int temp = array[comp];
+                    array[comp] = array[comp+1];
+                    array[comp+1] = temp;
+                }
+            }
+        }
+    }
 }
